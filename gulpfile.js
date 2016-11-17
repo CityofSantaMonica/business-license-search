b4e3d448a6bd1fdd0d88f77264703fe31c08b4c4
Kegan Maher <kegan.maher@smgov.net>
2016-11-17 15:01:43 -0800

    use gulp to pre-package all required assets
      - combine minified vendor files (angular, ng-table, etc.)
      - combine and minify js source
      - pre-compile templates into js

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,38 @@
 
 var gulp = require("gulp"),
     concat = require("gulp-concat"),
+    html2js = require("gulp-html2js"),
+    merge = require("merge-stream"),
     rename = require("gulp-rename"),
     uglify = require("gulp-uglify");
+
+var src = [
+    "node_modules/angular/angular.min.js",
+    "node_modules/ng-csv/build/ng-csv.min.js",
+    "node_modules/ng-table/bundles/ng-table.min.js",
+    "src/index.js",
+    "src/*.js"
+];
 
 var dest = "dist/";
 
 gulp.task("default", function() {
-  return gulp.src(["src/index.js", "src/*.js"])
-    .pipe(concat("business-license-search.js"))
-    .pipe(gulp.dest(dest))
-    .pipe(uglify())
-    .pipe(rename({ extname: ".min.js" }))
-    .pipe(gulp.dest(dest));
+    var templates = gulp.src("src/*.html")
+        .pipe(html2js("business-license-search.templates.js", {
+            adapter: "angular",
+            name: "businessLicenseSearch",
+        }))
+        .pipe(gulp.dest(dest))
+        .pipe(uglify())
+        .pipe(rename({ extname: ".min.js" }))
+        .pipe(gulp.dest(dest));
+
+    var js = gulp.src(src)
+        .pipe(concat("business-license-search.js"))
+        .pipe(gulp.dest(dest))
+        .pipe(uglify())
+        .pipe(rename({ extname: ".min.js" }))
+        .pipe(gulp.dest(dest));
+
+    return merge(js, templates);
 });
